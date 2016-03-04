5d7c2916f6d850bfddab56d1f53af3443756b7cf
Ryan N Johnson <ryan@discoveryhill.co>
2016-03-03 17:10:32 -0800

    init Seeds

---
+++ token_cloak/seeds.py
@@ -0,0 +1,19 @@
+
+
+class Seed:
+    """Basic seed object.
+    
+    The seed's enforces and describes the limits and available strings
+    used for seeding. Descendants simply change how seeds are ingested.
+    """
+    
+    def __init__(self):
+        """Setup some basic variables."""
+        pass
+        
+
+class SeedList(Seed):
+    """
+    
+    """
+    pass
