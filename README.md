dbc8e754350c50040f008fbd59b756b13a8e4ef1
Ryan N Johnson <ryan@discoveryhill.co>
2016-03-08 17:46:51 -0800

    travis setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # token-cloak-python
+
+[![Build Status](https://travis-ci.org/ryannjohnson/token-cloak-python.svg?branch=master)](https://travis-ci.org/ryannjohnson/token-cloak-python)
+
 A utility to lace public tokens with arbitrary data.
--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
             'bitarray',
         ],
         classifiers=[
-            'Development Status :: 3 - Alpha',
+            'Development Status :: 4 - Beta',
             'Environment :: Web Environment',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
